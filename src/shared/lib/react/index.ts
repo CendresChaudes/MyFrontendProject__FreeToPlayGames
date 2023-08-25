b70ad2340f5b3d9173bacac14b9de767d2d082b5
CendresChaudes <romqaaa1337@gmail.com>
2023-08-25 19:41:07 +0300

    service: подключает useBreakpoint

--- src/shared/lib/react/index.ts
+++ src/shared/lib/react/index.ts
@@ -1,3 +1,4 @@
 export { useOutsideClick } from './hooks/useOutsideClick';
 export { useAppDispatch } from './hooks/useAppDispatch';
 export { useAppSelector } from './hooks/useAppSelector';
+export { useBreakpoint } from './hooks/useBreakpoint';
