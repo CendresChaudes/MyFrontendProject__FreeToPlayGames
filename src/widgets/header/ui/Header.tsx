51b86cb6c3b244f948b75fbdf73bffd130a788f5
CendresChaudes <romqaaa1337@gmail.com>
2023-08-26 13:28:53 +0300

    feat: реализует переход на рут по клику кнопки

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,8 +1,9 @@
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import { Layout, Button, Divider, Row, Col } from 'antd';
-import { useBreakpoint } from '@/shared/lib';
+import { redirectToRoute, useAppDispatch, useBreakpoint } from '@/shared/lib';
 import { Logo } from '@/shared/ui';
 import styles from './styles.module.css';
+import { AppRoute } from '@/const';
 
 type HeaderProps = {
   hasReturnButton?: boolean;
@@ -11,25 +12,30 @@ type HeaderProps = {
 const { Header: _Header } = Layout;
 
 export function Header({ hasReturnButton = true }: HeaderProps) {
+  const dispatch = useAppDispatch();
   const currentBreakpoint = useBreakpoint();
 
   return (
     <>
       <_Header className={styles.header}>
         <Row className={styles.row} align="middle">
-          <Col xs={{ span: hasReturnButton ? 2 : 0 }} sm={{ span: 8 }}>
+          <Col xs={{ span: hasReturnButton ? 4 : 0 }} sm={{ span: 8 }}>
             {hasReturnButton && (
               <Button
                 type="primary"
+                shape="round"
                 icon={<ArrowLeftOutlined />}
                 aria-label="Return"
+                onClick={() => {
+                  dispatch(redirectToRoute(AppRoute.Root));
+                }}
               >
                 {currentBreakpoint !== 'xs' && 'Return'}
               </Button>
             )}
           </Col>
 
-          <Col xs={{ span: hasReturnButton ? 22 : 24 }} sm={{ span: 8 }}>
+          <Col xs={{ span: hasReturnButton ? 20 : 24 }} sm={{ span: 8 }}>
             <Logo className={styles.logo} />
           </Col>
         </Row>
