d0394a47610a2bb8bb8a5fd6b856e6b1c53a281c
CendresChaudes <romqaaa1337@gmail.com>
2023-08-26 12:34:14 +0300

    refactor: рефакторит 404 страницу
    * Центрирует контент
    * Добавляет кнопке ссылку на главную страницу

--- src/pages/error/notFound/ui/NotFoundPage.tsx
+++ src/pages/error/notFound/ui/NotFoundPage.tsx
@@ -1,12 +1,20 @@
 import { Button, Result } from 'antd';
+import { Link } from 'react-router-dom';
+import styles from './styles.module.css';
+import { AppRoute } from '@/const';
 
 export function NotFoundPage() {
   return (
     <Result
+      className={styles.result}
       status="404"
       title="404"
       subTitle="Sorry, the page you visited does not exist."
-      extra={<Button type="primary">Back Home</Button>}
+      extra={
+        <Button type="primary" shape="round">
+          <Link to={AppRoute.Root}>Back Home</Link>
+        </Button>
+      }
     />
   );
 }
