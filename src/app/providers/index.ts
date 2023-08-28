16a9d46174afe5a5c409dcf8b2a05889e6760b5b
CendresChaudes <romqaaa1337@gmail.com>
2023-08-28 15:58:44 +0300

    feat: настраивает отлов ошибок

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,6 +1,7 @@
 import compose from 'compose-function';
+import { withErrorBoundary } from './withErrorBoundary';
 import { withHelmet } from './withHelmet';
 import { withRedux } from './withRedux';
 import { withRouter } from './withRouter';
 
-export const withProviders = compose(withHelmet, withRedux, withRouter);
+export const withProviders = compose(withHelmet, withErrorBoundary, withRedux, withRouter);
