f33fd755b67ba3453582db2bf303316dbc6e1165
CendresChaudes <romqaaa1337@gmail.com>
2023-08-28 16:01:23 +0300

    fix: убирает импорт стилей удаленной библиотеки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { ScrollToTop } from '@/shared/lib';
 import { AppRouter } from './AppRouter';
 import { withProviders } from './providers';
-import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
