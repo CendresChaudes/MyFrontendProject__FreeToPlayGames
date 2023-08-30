74de2654e6c8ed48cc03d8316d6c1ae790070497
CendresChaudes <romqaaa1337@gmail.com>
2023-08-30 21:00:16 +0300

    fix: исправляет поломку приложения

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,24 @@
 
 import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: '/',
-    port: 3001
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/')
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL)
     },
-  },
+    server: {
+      open: '/',
+      port: 3001
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src/')
+      },
+    },
+  };
 });
