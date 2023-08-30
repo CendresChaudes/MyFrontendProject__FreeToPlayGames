70878b0e9fb055336fb7f68d07b2a08dfbe00adc
CendresChaudes <romqaaa1337@gmail.com>
2023-08-30 16:33:03 +0300

    service: подготавливает проект к тестированию

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
