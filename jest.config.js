828459eedfe37a49c95d45c2a920e46f5c37a4bc
CendresChaudes <romqaaa1337@gmail.com>
2023-08-30 18:39:58 +0300

    tests: настраивает расчет покрытия тестами

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest',
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/node_modules/**',
+    '!**/vendor/**',
+    '!**/index.{ts,tsx}',
+    '!**/const.ts',
+    '!**/types.d.ts',
+    '!**/*.config.ts'
+  ],
   testEnvironment: 'node',
   globals: {
     'ts-jest': {
