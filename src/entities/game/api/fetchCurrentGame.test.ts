ce695f1e9bd1d92400768f6ca85af10b0696323b
CendresChaudes <romqaaa1337@gmail.com>
2023-10-20 15:21:05 +0300

    tests: добавляет тесты для асинхронных экшенов

---
+++ src/entities/game/api/fetchCurrentGame.test.ts
@@ -0,0 +1,141 @@
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { Action, ThunkDispatch } from '@reduxjs/toolkit';
+import MockAdapter from 'axios-mock-adapter';
+import thunk from 'redux-thunk';
+import { createAPI } from '@/shared/api';
+import * as storageLibs from '@/shared/lib/storage';
+import { createMockCurrentGameData } from '../tests-lib/createMockCurrentGameData';
+import { fetchCurrentGame } from './fetchCurrentGame';
+import { APIRoute } from '@/const';
+
+jest.mock('@/shared/lib/storage');
+
+jest.mock('./adaptCurrentGameFromAPI', () => ({
+  adaptCurrentGameFromAPI: (item: GameAdaptedType) => item,
+}));
+
+type AppThunkDispatch = ThunkDispatch<State, ReturnType<typeof createAPI>, Action>;
+
+describe('Async action: fetchCurrentGame', () => {
+  const api = createAPI();
+  const mockAPIAdapter = new MockAdapter(api);
+  const middlewares = [thunk.withExtraArgument(api)];
+  const mockStoreCreator = configureMockStore<State, Action<string>, AppThunkDispatch>(middlewares);
+  const mockStore: ReturnType<typeof mockStoreCreator> = mockStoreCreator({ game: { games: [] } });
+
+  const mockCurrentGameData = createMockCurrentGameData();
+  const mockParams: FetchCurrentGameData = { id: 1 };
+
+  const mockExpiredCacheData = {
+    data: mockCurrentGameData,
+    cacheExpireTime: Date.now() - 10000000,
+  };
+
+  const mockNotExpiredCacheData = {
+    data: mockCurrentGameData,
+    cacheExpireTime: Date.now() + 10000000,
+  };
+
+  const getItemFromCache = storageLibs.getItemFromCache as jest.Mock;
+  const removeItemFromCache = storageLibs.removeItemFromCache as jest.Mock;
+  const setItemToCache = storageLibs.setItemToCache as jest.Mock;
+
+  beforeEach(() => {
+    getItemFromCache.mockClear();
+    removeItemFromCache.mockClear();
+    setItemToCache.mockClear();
+    mockAPIAdapter.history.get = [];
+  });
+
+  test('Should return a current game data from fetch if an item does not exist', async () => {
+    getItemFromCache.mockReturnValue(undefined);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    const currentGameFetchData = await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(currentGameFetchData.payload).toEqual(mockCurrentGameData);
+    expect(mockAPIAdapter.history.get.length).toBe(1);
+  });
+
+  test('Should return a current game data from fetch if an item exists and cache time has expired', async () => {
+    getItemFromCache.mockReturnValue(mockExpiredCacheData);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    const currentGameFetchData = await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(currentGameFetchData.payload).toEqual(mockCurrentGameData);
+    expect(mockAPIAdapter.history.get.length).toBe(1);
+  });
+
+  test('Should return a current game data from cache if an item exists and cache time has not expired', async () => {
+    getItemFromCache.mockReturnValue(mockNotExpiredCacheData);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    const currentGameFetchData = await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(currentGameFetchData.payload).toEqual(mockCurrentGameData);
+    expect(mockAPIAdapter.history.get.length).toBe(0);
+  });
+
+  test('should call "getItemFromCache" once', async () => {
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(getItemFromCache).toBeCalledTimes(1);
+  });
+
+  test('should not call "removeItemFromCache" if an item does not exist', async () => {
+    getItemFromCache.mockReturnValue(undefined);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(removeItemFromCache).not.toBeCalled();
+  });
+
+  test('should not call "removeItemFromCache" if an item exists and cache time has not expired', async () => {
+    getItemFromCache.mockReturnValue(mockNotExpiredCacheData);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(removeItemFromCache).not.toBeCalled();
+  });
+
+  test('should call "removeItemFromCache" once if an item exists and cache time has expired', async () => {
+    getItemFromCache.mockReturnValue(mockExpiredCacheData);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(removeItemFromCache).toBeCalledTimes(1);
+  });
+
+  test('should call "setItemToCache" once if an item does not exist', async () => {
+    getItemFromCache.mockReturnValue(undefined);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(setItemToCache).toBeCalledTimes(1);
+  });
+
+  test('should call "setItemToCache" once if an item exists and cache time has expired', async () => {
+    getItemFromCache.mockReturnValue(mockExpiredCacheData);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(setItemToCache).toBeCalledTimes(1);
+  });
+
+  test('should not call "setItemToCache" if an item exists and cache time has not expired', async () => {
+    getItemFromCache.mockReturnValue(mockNotExpiredCacheData);
+    mockAPIAdapter.onGet(APIRoute.Game).reply(200, mockCurrentGameData);
+
+    await mockStore.dispatch(fetchCurrentGame(mockParams));
+
+    expect(setItemToCache).not.toBeCalled();
+  });
+});
---
+++ src/entities/game/api/fetchGames.test.ts
@@ -0,0 +1,41 @@
+import { configureMockStore } from '@jedmao/redux-mock-store';
+import { Action, ThunkDispatch } from '@reduxjs/toolkit';
+import MockAdapter from 'axios-mock-adapter';
+import thunk from 'redux-thunk';
+import { createAPI } from '@/shared/api';
+import { Platform, Genre, SortType } from '../const';
+import { createMockGamesData } from '../tests-lib/createMockGamesData';
+import { fetchGames } from './fetchGames';
+import { APIRoute } from '@/const';
+
+jest.mock('./adaptGamesFromAPI', () => ({
+  adaptGamesFromAPI: (items: GamesAdaptedType[]) => items,
+}));
+
+type AppThunkDispatch = ThunkDispatch<State, ReturnType<typeof createAPI>, Action>;
+
+describe('Async action: fetchGames', () => {
+  const api = createAPI();
+  const mockAPIAdapter = new MockAdapter(api);
+  const middlewares = [thunk.withExtraArgument(api)];
+  const mockStoreCreator = configureMockStore<State, Action<string>, AppThunkDispatch>(middlewares);
+  const mockStore: ReturnType<typeof mockStoreCreator> = mockStoreCreator({ game: { games: [] } });
+
+  test('Should return an array of games data', async () => {
+    const mockGamesData = createMockGamesData();
+
+    const mockParams: FetchGamesData = {
+      params: {
+        platform: Platform.All,
+        category: Genre.All,
+        'sort-by': SortType.Relevance,
+      },
+    };
+
+    mockAPIAdapter.onGet(APIRoute.Games).reply(200, mockGamesData);
+
+    const gamesFetchData = await mockStore.dispatch(fetchGames(mockParams));
+
+    expect(gamesFetchData.payload).toEqual(mockGamesData);
+  });
+});
